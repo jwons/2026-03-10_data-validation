16090c0ac081e60c76835f542168afe99830b8f4
Joseph Wonsil <joe.wonsil@gmail.com>
2026-03-11 16:15:43 -0700

    examples from class for pointblank

--- src/validate.R
+++ src/validate.R
@@ -1,3 +1,14 @@
 library(pointblank)
+
 data(small_table)
 small_table
+
+agent <- small_table |>
+  create_agent() |>
+  col_vals_lt(a, value = 7) |>
+  col_vals_between(d, left = 0, right = 5000) |>
+  col_vals_in_set(f, set = c("low", "mid")) |>
+  col_vals_regex(b, regex = "^[0-9]-[a-z]{3}-[0-9]{3}$") |>
+  interrogate()
+
+get_sundered_data(agent, type = "combined")
